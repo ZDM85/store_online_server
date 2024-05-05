58efb65f5085d7b46c09c916b5bab97043cee67f
Dmitry Zabanov <110913076+ZDM85@users.noreply.github.com>
2024-05-05 18:53:35 +0800

    server refresh

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ module.exports = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    dialect: "/var/run/postgresql",
+    dialect: "postgres",
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
   }
