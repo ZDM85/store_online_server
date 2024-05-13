37ee0329dcaae0a5dbd053f171b4612b518bd85a
Dmitry Zabanov <110913076+ZDM85@users.noreply.github.com>
2024-05-13 10:31:44 +0800

    set .env

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 const { Sequelize } = require("sequelize");
+const { PostgresDialect } = require("@sequelize/postgres");
 
 module.exports = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    dialect: "postgres",
+    dialect: PostgresDialect,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
   }
